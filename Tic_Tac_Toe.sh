d30e4f2f0d86229b5a00adc249802642af7a4f62
anubhav230 <anubhavsingh230@gmail.com>
2020-06-10 20:40:24 +0530

    Printing game board

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -1,3 +1,15 @@
-#!/bin/bash -x
-echo "Welcome to the tic tac toe game"
-
+#!/bin/bash
+declare -a board
+count=0
+boradPrint() {
+	for place in {1..9}
+	do
+		board[((count++))]=$place
+	done
+	echo "${board[0]} | ${board[1]} | ${board[2]}"
+	echo ---------
+	echo "${board[3]} | ${board[4]} | ${board[5]}"
+	echo ---------
+	echo "${board[6]} | ${board[7]} | ${board[8]}"
+}
+boradPrint
