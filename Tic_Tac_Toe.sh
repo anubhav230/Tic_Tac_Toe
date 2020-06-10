3c1f935bb193651b25a527486562edfa43b32f3f
anubhav230 <anubhavsingh230@gmail.com>
2020-06-11 02:15:45 +0530

    Player starts playing

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
-emptyPlace="*"
+s#!/bin/bash
+
+flage=1
 declare -a board
 player=1
 system=0
 count=0
 #Printing Board
 boardPrint() {
-	for place in {1..9}
-	do
-		board[((count++))]=$emptyPlace
-	done
+	#for place in {1..9}
+	#do
+		#board[((count++))]=
+	#done
 	echo "${board[0]} | ${board[1]} | ${board[2]}"
 	echo ---------
 	echo "${board[3]} | ${board[4]} | ${board[5]}"
@@ -30,7 +31,7 @@ firstToss() {
 symbolAssigning() {
 	if [ $toss -eq $player ]
 	then
-		read -p "Please select symbol (x / o) for playing game" playerSymbol
+		read -p "Please select symbol (x / o) for playing game: " playerSymbol
 		if [ "$playerSymbol" == "o" ]
 		then
 			systemSymbol="x"
@@ -49,10 +50,45 @@ symbolAssigning() {
 		fi
 	fi
 }
-boardPrint
+#boardPrint
 firstToss
 symbolAssigning
 
 echo "system symbol = $systemSymbol"
 echo "player symbol = $playerSymbol"
+
+while (( flage >= 1 ))
+do
+	if [ $toss -eq $player ]
+	then
+		boardPrint
+		read -p "please enter position where you want to put symbol: " playerPosition
+		board[$playerPosition]=$playerSymbol
+		boardPrint
+	else
+		systemPosition=$((RANDOM%9+1))
+		board[$systemPosition]=$systemSymbol
+		boardPrint
+	fi
+	flage=0
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
