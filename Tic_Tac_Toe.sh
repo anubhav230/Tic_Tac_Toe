e3a6dd92ddfdf68a635c8043d2a2f3f7bd38e79a
anubhav230 <anubhavsingh230@gmail.com>
2020-06-11 01:06:39 +0530

    Assigning symbol for system and player

--- Tic_Tac_Toe.sh
+++ Tic_Tac_Toe.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+emptyPlace="*"
 declare -a board
 player=1
 system=0
 count=0
-boradPrint() {
+#Printing Board
+boardPrint() {
 	for place in {1..9}
 	do
-		board[((count++))]=$place
+		board[((count++))]=$emptyPlace
 	done
 	echo "${board[0]} | ${board[1]} | ${board[2]}"
 	echo ---------
@@ -14,11 +16,43 @@ boradPrint() {
 	echo ---------
 	echo "${board[6]} | ${board[7]} | ${board[8]}"
 }
-boradPrint
-toss=$((RANDOM%2))
-if [ $toss -eq $player ]
-then
-	echo "Player won the toss and have chance to play first"
-else
-	echo "System won the toss and have chence to play first"
-fi
+#Toss for first play
+firstToss() {
+	toss=$((RANDOM%2))
+	if [ $toss -eq $player ]
+	then
+		echo "Player won the toss and have chance to play first"
+	else
+		echo "System won the toss and have chence to play first"
+	fi
+}
+#assiginig symbol for player and system
+symbolAssigning() {
+	if [ $toss -eq $player ]
+	then
+		read -p "Please select symbol (x / o) for playing game" playerSymbol
+		if [ "$playerSymbol" == "o" ]
+		then
+			systemSymbol="x"
+		else
+			systemSymbol="o"
+		fi
+	else
+		systemSymbolToss=$((RANDOM%2))
+		if [ $systemSymbolToss -eq 1 ]
+		then
+			systemSymbol="o"
+			playerSymbol="x"
+		else
+			systemSymbol="x"
+			playerSymbol="o"
+		fi
+	fi
+}
+boardPrint
+firstToss
+symbolAssigning
+
+echo "system symbol = $systemSymbol"
+echo "player symbol = $playerSymbol"
+
